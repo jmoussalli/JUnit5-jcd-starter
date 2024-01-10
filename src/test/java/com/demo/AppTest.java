66c768f3f0924c4201a186ebf05a849d9c47e5d7
jmoussalli <jerome.moussalli@gmail.com>
2024-01-10 10:49:35 +0100

    First Commit

--- src/test/java/com/demo/AppTest.java
+++ src/test/java/com/demo/AppTest.java
@@ -1,7 +1,8 @@
 package com.demo;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest
 {
